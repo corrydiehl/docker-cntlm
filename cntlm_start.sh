0f38d357baab835923f6db27a7f0a6aeb474e83f
Corry Diehl <corry@fedora.plasma>
2017-04-18 19:19:36 -0400

    Adding security with allow/deny to the cntlm proxy

--- cntlm_start.sh
+++ cntlm_start.sh
@@ -9,6 +9,11 @@ echo PassNTLMv2 $3 >> /etc/cntlm.ini
 echo Proxy $4 >> /etc/cntlm.ini
 echo NoProxy localhost, 127.0.0.*, 10.*, 192.168.*, *.$2 >> /etc/cntlm.ini
 echo Gateway yes >> /etc/cntlm.ini
+# This denies all ip addresses outright
+echo Deny 0/0 >> /etc/cntlm.ini
+# This allows a specific ip address or range of addresses
+# For multiple "Allow"s, add them each one on a new line
+echo Allow 172.17.0.0/24
 echo Listen 3128 >> /etc/cntlm.ini
 
 exec /usr/sbin/cntlm -f -U cntlm -c /etc/cntlm.ini
